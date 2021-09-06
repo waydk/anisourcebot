a72824ef0d0416b7cc5d1e7e3af3098e0cb7475a
waydk <waydk.main@gmail.com>
2021-09-06 15:32:25 +0800

    0.0.3: saucenao: anime. art

--- tgbot/modules/__init__.py
+++ tgbot/modules/__init__.py
@@ -1,7 +1,7 @@
 from tgbot.modules.search import search_source
 from tgbot.modules.start import start_command
-from aiogram import Dispatcher
+from aiogram import Dispatcher, types
 
 def setup(dp: Dispatcher):
     dp.register_message_handler(start_command, commands="start")
-    dp.register_message_handler(search_source, commands="search")
+    dp.register_message_handler(search_source, content_types=types.ContentType.PHOTO)
--- tgbot/modules/search.py
+++ tgbot/modules/search.py
@@ -1,12 +1,47 @@
+import os
+
+from aiogram import Bot
 from environs import Env
 from aiogram.types import Message
 from PicImageSearch import AsyncSauceNAO, NetWork
 
+
 async def search_source(message: Message):
+    """The function gets the message,
+     takes the photo id from it and downloads
+     the photo, then searches for the original source
+
+    Args:
+        message (Message): aiogram.types.Message
+    """
+    # Get token and sauce api from env
     env = Env()
     env.read_env()
+
+    token = env.str("BOT_TOKEN")
     api_key = env.str("SAUCE_API")
+
+    photo_path = os.path.join('tgbot', 'photos', 'search_photo.jpg')
+
+    #  Download photo
+    bot = Bot(token=token, parse_mode='html')
+    file_id = message.photo[-1].file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+
+    await bot.download_file(file_path, photo_path)
+
     async with NetWork() as client:
         saucenao = AsyncSauceNAO(client=client, api_key=api_key)
-        res = await saucenao.search('https://pixiv.cat/77702503-1.jpg')
-        await message.answer(res.raw[0].title)
+        res = await saucenao.search(photo_path)
+        try:
+            est_time = f"☁ Est Time: {res.origin['results'][0]['data']['est_time']}"
+        except KeyError:
+            est_time = ''
+        res = res.raw[0]
+        author = f"☁ Author: {res.author}\n" if res.author else ''
+    await message.answer(f"☁ Title: {res.title}\n"
+                         f"☁ Similarity: {res.similarity}%\n"
+                         f"{author}"
+                         f"{est_time}"
+                         f"☁ Url: {res.url}")
