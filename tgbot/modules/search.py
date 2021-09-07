c529074630e58ef538f7e072717f4f0f68c9a340
waydk <waydk.main@gmail.com>
2021-09-07 11:42:31 +0800

    0.0.3: some fixes

--- tgbot/modules/search.py
+++ tgbot/modules/search.py
@@ -4,6 +4,39 @@ from aiogram import Bot
 from environs import Env
 from aiogram.types import Message
 from PicImageSearch import AsyncSauceNAO, NetWork
+from loguru import logger
+
+async def saucenao_search(message: Message, api_key: str, photo_path: str):
+    """Api saucenao search
+
+    Args:
+        message (aiogram.types.Message): sending a message to a user
+        api_key (str): to access the api
+        photo_path (str): the path to the photo that is searched for
+    """
+    async with NetWork() as client:
+        saucenao = AsyncSauceNAO(client=client, api_key=api_key)
+        res = await saucenao.search(photo_path)
+
+        try:
+            est_time = f"☁ Est Time: {res.origin['results'][0]['data']['est_time']}\n"
+        except KeyError:
+            est_time = ''
+
+        try:
+            part = f"☁ Part: {res.origin['results'][0]['data']['part']}\n"
+        except KeyError:
+            part = ''
+
+        res = res.raw[0]
+        author = f"☁ Author: {res.author}\n" if res.author else ''
+
+    await message.answer(f"☁ Title: {res.title}\n"
+                         f"☁ Similarity: {res.similarity}%\n"
+                         f"{author}"
+                         f"{est_time}"
+                         f"{part}"
+                         f"☁ Url: {res.url}")
 
 
 async def search_source(message: Message):
@@ -31,17 +64,7 @@ async def search_source(message: Message):
 
     await bot.download_file(file_path, photo_path)
 
-    async with NetWork() as client:
-        saucenao = AsyncSauceNAO(client=client, api_key=api_key)
-        res = await saucenao.search(photo_path)
-        try:
-            est_time = f"☁ Est Time: {res.origin['results'][0]['data']['est_time']}"
-        except KeyError:
-            est_time = ''
-        res = res.raw[0]
-        author = f"☁ Author: {res.author}\n" if res.author else ''
-    await message.answer(f"☁ Title: {res.title}\n"
-                         f"☁ Similarity: {res.similarity}%\n"
-                         f"{author}"
-                         f"{est_time}"
-                         f"☁ Url: {res.url}")
+    try:
+        await saucenao_search(message, api_key, photo_path)
+    except Exception as err:
+        logger.debug(err)
--- tgbot/modules/start.py
+++ tgbot/modules/start.py
@@ -1,16 +1,25 @@
+from aiogram.types.inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup
 from loguru import logger
 from aiogram.types import Message
 
 
 async def start_command(message: Message):
+    """Bot command /start
+
+    Args:
+        message (Message): To send a message to a user
     """
-    Responds to /start
-    :param message:
-    :return:
-    """
+    keyboard = InlineKeyboardMarkup(row_width=3)
+
+    keyboard.add(
+        InlineKeyboardButton('github', url='https://github.com/waydk/anisourcebot')
+    )
+
     user = message.from_user.full_name
     logger.info(f"{user} send /start")
     await message.answer(
         f"Hi, <b>{user}</b>, this bot was created"
-        "to find the original source of anime, manga and art."
+        " to find the original source of anime, manga and art.\n\n"
+        "<i>Just send a photo.</i>",
+        reply_markup=keyboard
     )
